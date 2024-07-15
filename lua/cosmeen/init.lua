f995ce7cfa773e7cf022c445268dfaa882b4f6db
Cosmeeeen <iliemcosmin@yahoo.ro>
2024-07-15 12:36:38 +0300

    setup lsp

--- lua/cosmeen/init.lua
+++ lua/cosmeen/init.lua
@@ -1,5 +1,3 @@
 require("cosmeen.lazy")
 require("cosmeen.remap")
 
-print("config successfully loaded :3")
-
--- lua/cosmeen/lazy.lua
+++ lua/cosmeen/lazy.lua
@@ -47,7 +47,14 @@ require("lazy").setup({
     },
     {
 	    "tpope/vim-fugitive"
-    }
+    },
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'}
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
