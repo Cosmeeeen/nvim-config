5f52c335dfafb5a72833c542b2f68707880a373f
Cosmeeeen <iliemcosmin@yahoo.ro>
2024-07-16 07:17:08 +0300

    remove background from file browser

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,4 +1,9 @@
-vim.keymap.set("n", "<leader>pv", ":Neotree filesystem reveal current<CR>")
+vim.cmd([[
+  highlight NeoTreeNormal guibg=none
+  highlight NeoTreeNormalNC guibg=none
+]])
+
+vim.keymap.set("n", "<leader>pv", ":Neotree filesystem reveal<CR>", { silent = true })
 
 require('neo-tree').setup({
   hijack_netrw_behavior = "open_current",
