5f614e06f8324e019a2bdc9ebb50c5f6b3e59ad9
Cosmeeeen <iliemcosmin@yahoo.ro>
2024-08-12 11:56:12 +0300

    <leader>ld shows line diagnostics in a popup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,6 +5,8 @@ lsp_zero.on_attach(function(client, bufnr)
     -- to learn the available actions
     local opts = { buffer = bufnr, remap = false }
 
+    vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
+
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
