e09211b4f58224e961b204a327641c622d2f9531
Cosmeeeen <iliemcosmin@yahoo.ro>
2025-04-08 12:57:39 +0300

    fix harpoon save_on_toggle

--- lua/cosmeen/remap.lua
+++ lua/cosmeen/remap.lua
@@ -14,8 +14,8 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.api.nvim_create_user_command("Cwd", function()
-    local path = vim.fn.expand("%:p")
-    vim.fn.setreg("+", path)
-    vim.notify('Copied "' .. path .. '" to the clipboard!')
+  local path = vim.api.nvim_buf_get_name(0)
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
 
